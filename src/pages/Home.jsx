c0f74b05146cad3e19741fc8d9705475f43efd97
rkarthik-iamok <rkarthik.iamok@gmail.com>
2024-06-26 10:20:37 -0400

    Finished with the DR Demo App in React

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,6 +44,12 @@ const Home = () => {
     AP: "https://ciam-spoke01.karthiktc.com/app/okta_org2org/exkcgsgxn3ZpTPsgJ697/sso/saml?RelayState",
   };
 
+  const regionalSignupLinks = {
+    EU: "https://ciam-spoke02.karthiktc.com/app/bookmark/0oafxf26x71vfBnLb697/login?signup_page=true",
+    US: "https://ciam-hub.karthiktc.com/oauth2/default/v1/authorize?client_id=0oafuyhwb5JyOMrzt697&code_challenge=Z6zu-ZZWHMn0XoDOgMPTfqCZZ7RT161vEeQTj5JQkFI&code_challenge_method=S256&nonce=FgKN9IduBMELstBSwQfQ3HdiSQ4IBohRU3MuUKsbdjI8tVj3q8z2ZB7s5YSrp5WT&redirect_uri=https%3A%2F%2Fglobal-app02.karthiktc.com%2Flogin%2Fcallback&response_type=code&state=UeDaV3JbdX60xplCbPB3Z2IQIHhoOtf9PqzUJKSu8hZRKBTNTKhAaaKXZD0swRHT&scope=openid%20profile%20email&signup_page=true",
+    AP: "https://ciam-spoke01.karthiktc.com/app/bookmark/0oafxhvjafaxHfQre697/login?signup_page=true",
+  };
+
   useEffect(() => {
     if (!authState || !authState.isAuthenticated) {
       // When user isn't authenticated, forget any user info
@@ -105,6 +111,15 @@ const Home = () => {
   const signup = async () => {
     window.location.href =
       "https://ciam-spoke02.karthiktc.com/oauth2/default/v1/authorize?client_id=0oac2qpybeHSxXr7T697&code_challenge=AptwK4s28Vrla9GDeSLhE9QEDT_p41ZihtfDKgeKU2A&code_challenge_method=S256&nonce=yuxWTTyxdoAd2RuPGxsPIkrevxnrYlhKjGq3Fdr34rgepSuCHRGiOhlPARa0H78K&redirect_uri=https%3A%2F%2Flocal-app-01.karthiktc.com%2Flogin%2Fcallback&response_type=code&state=qOvqq0XLGbIVpk1IZRoCkNC8JnxxBzzE02ZAd1FvnpkwGN5uQcWqYvUyL6UolXSK&acr_values=urn%3Aokta%3Aloa%3A1fa%3Apwd&scope=openid%20email%20profile%20offline_access&signup_page=true";
+  };
+
+  const signupDR = async () => {
+    const locale = navigator.language || navigator.userLanguage;
+    const region = localeToRegion[locale];
+
+    window.location.href = regionalSignupLinks[region];
+    // window.location.href =
+    //   "https://ciam-spoke02.karthiktc.com/oauth2/default/v1/authorize?client_id=0oac2qpybeHSxXr7T697&code_challenge=AptwK4s28Vrla9GDeSLhE9QEDT_p41ZihtfDKgeKU2A&code_challenge_method=S256&nonce=yuxWTTyxdoAd2RuPGxsPIkrevxnrYlhKjGq3Fdr34rgepSuCHRGiOhlPARa0H78K&redirect_uri=https%3A%2F%2Flocal-app-01.karthiktc.com%2Flogin%2Fcallback&response_type=code&state=qOvqq0XLGbIVpk1IZRoCkNC8JnxxBzzE02ZAd1FvnpkwGN5uQcWqYvUyL6UolXSK&acr_values=urn%3Aokta%3Aloa%3A1fa%3Apwd&scope=openid%20email%20profile%20offline_access&signup_page=true";
   };
 
   if (!authState) {
@@ -195,13 +210,17 @@ const Home = () => {
                 Login DR (Browser Locale)
               </Button>
 
-              <Button id="login-button" primary onClick={loginSpoke01}>
+              <Button id="login-button" primary onClick={signupDR}>
+                Signup DR (Browser Locale)
+              </Button>
+
+              {/* <Button id="login-button" primary onClick={loginSpoke01}>
                 Login Spoke01
               </Button>
 
               <Button id="login-button" primary onClick={loginSpoke02}>
                 Login Spoke02
-              </Button>
+              </Button> */}
 
               {/* <Button id="login-button" primary onClick={loginSpoke02oidc}>
                 Login Spoke02 OIDC
